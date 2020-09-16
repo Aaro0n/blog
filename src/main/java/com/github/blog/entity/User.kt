115cf3aeab3ad9735e688e273760251e1d94d69a
Aaro0n <mrcard97@gmail.com>
2020-09-16 15:55:34 +0800

    add h2

--- src/main/java/com/github/blog/entity/User.kt
+++ src/main/java/com/github/blog/entity/User.kt
@@ -2,11 +2,12 @@ package com.github.blog.entity
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 class User(
-        @Id @GeneratedValue var id: Long? = null,
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
         var email: String? = null,
         var password: String? = null,
         var name: String? = null,
--- src/main/java/com/github/blog/entity/Visit.kt
+++ src/main/java/com/github/blog/entity/Visit.kt
@@ -2,13 +2,14 @@ package com.github.blog.entity
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 
 @Entity
 data class Visit(
         @Id
-        @GeneratedValue
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
         var visitTime: Long,
         var ipAddress: Long,
